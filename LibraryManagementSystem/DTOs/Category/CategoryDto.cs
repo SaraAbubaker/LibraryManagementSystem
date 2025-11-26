f6a38d75e206e0e7b65daf0411c3176dba992129
SaraAbubaker <qasrrawii@gmail.com>
2025-11-26 11:45:25 +0300

    Added Category DTOs

---
+++ LibraryManagementSystem/DTOs/Category/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.DTOs.Category
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        
+        public string? CreatedByUserId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        public string? LastModifiedByUserId { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
