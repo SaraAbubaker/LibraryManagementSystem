efaca3c9098f3ad94cba92c91bf207f95c13265b
SaraAbubaker <qasrrawii@gmail.com>
2025-11-26 15:40:41 +0300

    Added common search DTO

--- LibraryManagementSystem/DTOs/Book/SearchBookParamsDto.cs
+++ LibraryManagementSystem/DTOs/Book/SearchBookParamsDto.cs
@@ -4,17 +4,8 @@ using System.Text;
 
 namespace LibraryManagementSystem.DTOs.Book
 {
-    public class SearchBookParamsDto
+    public class SearchBookParamsDto : SearchParamsDto
     {
-        // Paging
-        public int Page { get; set; } = 1;
-        public int PageSize { get; set; } = 5;
-
-        // Sorting
-        public string? SortBy { get; set; } = "Title"; //Title, PublishDate, etc
-        public string? SortDir { get; set; } = "asc"; //asc or desc
-
-        // Search
         public string? Title { get; set; }
         public DateTime? PublishDate { get; set; }
         public string? Version { get; set; }
---
+++ LibraryManagementSystem/DTOs/SearchParamsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagementSystem.DTOs
+{
+    public class SearchParamsDto
+    {
+        // Paging
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+
+        // Sorting
+        public string? SortBy { get; set; } //Title, PublishDate, etc
+        public string? SortDir { get; set; } = "asc"; //asc or desc
+    }
+}
