f31451256cd8bc74d7ef2d569a503fa5019a2a03
SaraAbubaker <qasrrawii@gmail.com>
2025-12-01 00:39:29 +0300

    Added Borrow and Category Controllers

--- LibraryManagementSystemAPIs/Controllers/CategoryController.cs
+++ LibraryManagementSystemAPIs/Controllers/CategoryController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryManagementSystem.DTOs.Category;
+using LibraryManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
 
-namespace LibraryManagementSystemAPIs.Controllers
+namespace LibraryManagementSystem.Controllers
 {
-    public class CategoryController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly CategoryService Service;
+
+        public CategoryController(CategoryService service)
         {
-            return View();
+            Service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = Service.GetAllCategories();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryDto dto)
+        {
+            var created = Service.CreateCategory(dto);
+            return Ok(created);
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateCategoryDto dto)
+        {
+            var currentUserId = 1; //temp until authentication
+
+            var updated = Service.UpdateCategory(dto, currentUserId);
+
+            if (updated == null)
+                return NotFound("Category not found.");
+
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Archive(int id)
+        {
+            var currentUserId = 1; //temp
+
+            var success = Service.ArchiveCategory(id, currentUserId);
+
+            if (!success)
+                return NotFound("Category not found.");
+
+            return Ok("Category archived successfully.");
         }
     }
 }
