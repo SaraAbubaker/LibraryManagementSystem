3843ad0296f013263a006e7d5282ec10a1429349
SaraAbubaker <qasrrawii@gmail.com>
2025-12-02 12:26:22 +0300

    Added DbContext Class

---
+++ LibraryManagementSystem/Data/LibraryContext.cs
@@ -0,0 +1,36 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<InventoryRecord> InventoryRecords { get; set; }
+        public DbSet<BorrowRecord> BorrowRecords { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=LibraryDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 0, Name = "Unknown", Email = string.Empty }
+            );
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 0, Name = "Unknown" }
+            );
+
+            modelBuilder.Entity<Publisher>().HasData(
+                new Publisher { Id = 0, Name = "Unknown" }
+            );
+        }
+    }
+}
